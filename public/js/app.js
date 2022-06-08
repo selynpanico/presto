70fa90f22bdb48de9888ca3ff0f7ba9646c80524
Simone Carannante <simonecarannante@outlook.it>
2022-06-08 23:28:02 +0200

    Fixata responsive carosello home

--- public/js/app.js
+++ public/js/app.js
@@ -3258,16 +3258,16 @@ var swiper = new Swiper(".swiperAnnouncements", (_Swiper = {
   spaceBetween: 10,
   // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
+    // // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20
+    // },
+    // // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30
+    // },
     // when window width is >= 640px
     640: {
       slidesPerView: 4,
--- resources/js/swiperCarouselHome.js
+++ resources/js/swiperCarouselHome.js
@@ -5,16 +5,16 @@ var swiper = new Swiper(".swiperAnnouncements", {
   spaceBetween: 10,
     // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
+    // // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20
+    // },
+    // // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30
+    // },
     // when window width is >= 640px
     640: {
       slidesPerView: 4,
