56bd74430e9929cb1bd28bb92e10ee251aa2d191
Simone Carannante <simonecarannante@outlook.it>
2022-06-08 17:03:29 +0200

    Reso utente revisore da email per admin

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,17 @@
-<!-- <p>Sono un footer</p> -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-column align-items-center"> 
+                <h5>LAVORA CON NOI</h5>
+                <p>Se sei in cerca di un arrotondamento per lo stipendio da fame in italia</p>
+                <a href="{{route('became.revisor')}}" class="btn btn-warning">CLICCA QUI</a>
+             </div>
+        </div>
+    </div>
+    <x-bladewind.notification position="bottom right" />
+    @if(session()->has('message'))
+    <script>
+        showNotification('Success', "Canditatura inviata con successo", 'success', 3);
+    </script>   
+    @endif
+</footer>
