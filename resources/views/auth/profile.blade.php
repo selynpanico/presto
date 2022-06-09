ce18594bd4e2dfda4b96a04cd908a89d929634de
Simone Carannante <simonecarannante@outlook.it>
2022-06-09 16:42:14 +0200

    Aggiunte condizioni per articoli nel profilo

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -14,10 +14,6 @@
                     <a href="{{route('revisor-panel')}}" class="btn btn-warning">Vai al pannello revisore</a>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
             <div class="col-12 col-md-6">
                 <div class="position-relative">
                     <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
@@ -29,8 +25,36 @@
             </div>
         </div>
     </div>
-    
-@endif
+    @endif
+    <div class="container">
+        <div class="row">
+            @if(count(Auth::user()->announcements)>0)
+            @foreach(Auth::user()->announcements as $announcement)
+            @if($announcement->is_accepted)
+            <div class="col-12 col-md-4">
+                <div class="card shadow" style="width:18rem;">
+                    <img src="https://picsum.photos/200" class="card-img-top p-3 rounded"alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$announcement->title}}</h5>
+                        <pclass="card-text">{{$announcement->body}}</p>
+                    <a href="{{route('announcement.show',compact('announcement'))}}"class="btn
+                        btn-primary shadow">Visualizza </a>
+                        <a href="{{route('category.show', ['category'=>$announcement->category])}}"
+                        class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Categoria:{{$announcement->category->name}}</a>
+                        <pclass="card-footer">Pubblicato il:{{$announcement->created_at->format('d/m/Y')}}</p>
+                    </div>
+                </div>
+            </div>
+            @endif
+            @endforeach
+            @else
+            <x-bladewind.empty-state
+                message="Crea un annuncio o attendi che un revisore lo approvi">
+            </x-bladewind.empty-state>
+            @endif
+
+        </div>
+    </div>
 
 <!-- Da inserire tutti gli annunci dell'utente -->
 
