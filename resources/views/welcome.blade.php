76beaa66c1e5c0be34d3aba757d7f8b6bca85d1e
Simone Carannante <simonecarannante@outlook.it>
2022-06-09 15:20:02 +0200

    Customizzati bottoni carosello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,11 +69,11 @@
                         @endforeach
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon " aria-hidden="true"></span>
+                        <i class="bi bi-arrow-left-circle color1"></i>
                         <span class="visually-hidden">Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <i class="bi bi-arrow-right-circle color2"></i>
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
