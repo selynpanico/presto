8679c4eb16409bd3afc6365e7f8ac0b4c320057b
selyn panico <selynpanico@gmail.com>
2022-06-09 23:07:39 +0200

    fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,3 @@
-<footer class="mt-auto">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 d-flex flex-column align-items-center"> 
-                <h5>LAVORA CON NOI</h5>
-                <h6>Se sei in cerca di un arrotondamento per lo stipendio da fame in italia</h6>
-                <livewire:revisor-request-button />
-                <span class="my-2"></span>
-             </div>
-        </div>
-    </div>
-    
-    
     <div class="social-container"> 
         <div class="row m-0 justify-content-end">
 
