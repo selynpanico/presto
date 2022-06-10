98a72e5b735ff203edc48970496b98cde1e16490
Simone Carannante <simonecarannante@outlook.it>
2022-06-10 10:31:19 +0200

    Fixato minibug

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,11 @@
         <div class="col-12 d-flex flex-column align-items-center my-2 py-2 text-white text-center"> 
                 <h5>LAVORA CON NOI</h5>
                 <h6>Se sei in cerca di un arrotondamento per lo stipendio da fame in italia</h6>
+                @if(Auth::user())
                 <livewire:revisor-request-button />
+                @else
+                <a class="btn btn-warning" href="{{route('register')}}">Registrati per poter mandare la domanda</a>
+                @endif
                 
              </div>
 
