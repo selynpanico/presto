8f790cdfc63af4cb245fc0aadfdb890ef34a5efe
selyn panico <selynpanico@gmail.com>
2022-06-10 09:45:49 +0200

    fix btn footer

--- resources/views/livewire/revisor-request-button.blade.php
+++ resources/views/livewire/revisor-request-button.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if(!$is_sent)
-    <button wire:click='sendRequest' class="btn btn-light d-flex">clicca qui <img src="/img/icone/presto_icona-all.svg" alt="" width="20px"></button>
+    <button wire:click='sendRequest' class="btn btn-light d-flex justify-content-center align-items-center my-2">clicca qui <img class="ms-2" src="/img/icone/presto_icona-all.svg" alt="" width="20px"></button>
     @else
         <h2>Grazie per averci contattato</h2>
         <!-- <x-bladewind.notification position="bottom right" />
