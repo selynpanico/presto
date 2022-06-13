0412f79033120d574a15005039d6c6bf04f20687
Stefano Di Giovanni <stefano.digiovanni85@gmail.com>
2022-06-13 10:59:40 +0200

    fix responsive categorie

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -10,11 +10,26 @@
         </div>
     </div>
     <div class="row  justify-content-center">
-        <div class="col-12 col-md-6 d-flex"> 
-            <a href="{{ route('all.announcements')}}"><img class="m-2" src="/img/icone/presto_icona-all.svg" alt="tutte le categorie" width="100%"></a> 
-            @foreach($categories as $category)
-                <a href="{{route('category.show', $category)}}"><img class="m-2" src="{{ $category->icon}}" alt="{{ $category->name}}" width="100%"></a> 
-            @endforeach
+        <div class="col-12 col-md-6 d-flex">
+            <div class='d-none d-md-flex'>
+                <a href="{{ route('all.announcements')}}"><img class="m-2" src="/img/icone/presto_icona-all.svg" alt="tutte le categorie" width="100%"></a> 
+                @foreach($categories as $category)
+                    <a href="{{route('category.show', $category)}}"><img class="m-2" src="{{ $category->icon}}" alt="{{ $category->name}}" width="100%"></a> 
+                @endforeach
+            </div>
+            <div class='d-md-none'>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            Categoria
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    @foreach($categories as $category)
+                    <li><a class='dropdown-item' href="{{route('category.show', $category)}}"><img class="m-2" src="{{ $category->icon}}" alt="{{ $category->name}}" width="20%">{{$category->name}}</a></li>
+                @endforeach
+                    </ul>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
