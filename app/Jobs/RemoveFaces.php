1c58164b8fe1c061669cd795a8528b728f67c787
Simone Carannante <simonecarannante@outlook.it>
2022-06-14 17:24:02 +0200

    Iniziata user story 8

---
+++ app/Jobs/RemoveFaces.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class RemoveFaces implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $announcement_image_id;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($announcement_image_id)
+    {
+        $this->announcement_image_id = $announcement_image_id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $i = Image::find($this->announcement_image_id);
+        if(!$i){
+            return;
+        }
+    }
+}
