1743ca900b616031be65fc1944f7856ba382d3e8
selyn panico <selynpanico@gmail.com>
2022-06-15 22:16:37 +0200

    fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,15 @@
 <footer>
 <div class="social-container h-100"> 
     <div class="row m-0 justify-content-end">
-        <div class="col-12 d-flex flex-column align-items-center my-2 py-2 text-white text-center"> 
+        <div class="col-12 d-flex flex-column align-items-center my-3 py-2 text-white text-center"> 
             <h5>{{__('ui.work_for_us')}}</h5>
             <h6>{{__('ui.work_for_us_subtitle')}}</h6>
             @if(Auth::user() && Auth::user()->is_revisor)
-                <h2>Gia sei revisore</h2>
+                <h4 class="fs-6 mt-2">Gia sei revisore</h4>
                 @elseif(Auth::user())
                 <livewire:revisor-request-button />
              @else
-                <a class="btn btn-light" href="{{route('register')}}">{{__('ui.register_to_work')}}</a>
+                <a class="btn btn-light my-2" href="{{route('register')}}">{{__('ui.register_to_work')}}</a>
             @endif  
         </div>
         <div class="col-12 col-md-6">
