d1b7bbd8931b603171d54151294e3a26f244107e
selyn panico <selynpanico@gmail.com>
2022-06-15 21:43:04 +0200

    fix pagina dettaglio

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($contain)
-    <button wire:click="setInCart" class="btn btn-primary"><p class="text-danger">Rimuovi dal carrello</p><i class="text-danger bi bi-cart-dash"></i></button>
+    <button wire:click="setInCart" class="btn btn-secondary"><p class="text-white">Rimuovi dal carrello</p><i class="text-white bi bi-cart-dash"></i></button>
     
     @else
     <button wire:click="setInCart" class="btn btn-primary"><p class="text-white">Aggiungi al carrello</p><i class="text-white bi bi-cart-plus"></i></button>
