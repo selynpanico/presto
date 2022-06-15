ce829722b1873671a2bfb43de0d07d789e56bf4d
selyn panico <selynpanico@gmail.com>
2022-06-15 17:18:41 +0200

    pagina contatti

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,5 +1,37 @@
 <x-layout>
 
-<h1>CONTATTI</h1>
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <h2 class="text-center fw-bold my-5">i membri del team</h2>
+        <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
+            <div class="d-flex flex-column align-items-center"><img src="/img/contatti/presto-contatti-05.png" alt="" width="70%">
+                <h6 class="text-dark mt-4">follow me
+               <a href="https://www.instagram.com/selynpanico/"><i class="bi bi-instagram"></i> </a> 
+                <h3 class="mt-2 mb-5 fw-bold contact-text">selyn</h3>
+                </h6>
+            </div> 
+        </div>
+
+        <div class="col-12 col-md-4">
+        <div class="d-flex flex-column align-items-center"><img src="/img/contatti/presto-contatti-06.png" alt="" width="70%"> 
+                <h6 class="text-dark mt-4">follow me
+                <a href="https://www.instagram.com/selynpanico/"><i class="bi bi-instagram"></i> </a> 
+                <h3 class="mt-2 mb-5 fw-bold contact-text">simone</h3>
+                </h6>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
+        <div class="d-flex flex-column align-items-center"><img src="/img/contatti/presto-contatti-07.png" alt="" width="70%"> 
+                <h6 class="text-dark mt-4">follow me
+                <a href="https://www.instagram.com/selynpanico/"><i class="bi bi-instagram"></i> </a> 
+                <h3 class="mt-2 mb-5 fw-bold contact-text">stefano</h3>
+                </h6>
+            </div>
+        </div>
+
+        </div>
+    </div>
+</div>
 
 </x-layout>
